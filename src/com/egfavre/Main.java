70c0ce02e6b4417955ad11185afc7deb871b9c5b
Liz Favre <egfavre@gmail.com>
2016-06-06 09:55:45 -0400

    anonymous functions (lambda)

--- src/com/egfavre/Main.java
+++ src/com/egfavre/Main.java
@@ -17,21 +17,35 @@ public class Main {
         };
         a.name = "alligator";
 
+        //anonymous function or lambda- store method inside object
+        Runnable r = () -> {
+            System.out.println("Hello from lambda");
+        };
+        r.run();
+        //advantage is that you can pass the method like an object
+
         d.makeSound();
         s.makeSound();
         h.makeSound();
         a.makeSound();
 
+        sayHello(r);
 
         System.out.println(d);
         System.out.println(s);
         System.out.println(h);
         System.out.println(a);
 
-        //returns name instead of memory address
+        //returns name instead of memory address, can pass into another method
 
 
     }
+
+    public static void sayHello(Runnable runnable){
+        System.out.println("Hello");
+        runnable.run();
+    }
+
     //factory method- creates objects
     public static Animal createAnimal(String name){
         switch (name) {
